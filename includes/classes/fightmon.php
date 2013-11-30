a2a346648f0c91944ca6c7d56e76b6cb54ba28e1
iggyvolz <iggyvolz@aim.com>
2013-11-30 14:23:57 -0500

    Minor: Alphabetization

--- includes/classes/fightmon.php
+++ includes/classes/fightmon.php
@@ -87,6 +87,92 @@ class fightmon
 		return array('');
 	}
 
+	public function update($args = NULL)
+	{
+		global $folder;
+		if (!$_SESSION['ftgr']['valid_session'])
+		{
+			return array(FTGR_UPDATE_REJECT);
+		}
+		if ($args === 'NULL')
+		{
+			return array(FTGR_REQUIRED_PARAM);
+		}
+		$url = trim('http://fightmon.eternityincurakai.com/ftgr/' . $args[0] . '.zip');
+		$contents = file_get_contents($url);
+		file_put_contents(__DIR__ . FTGR_SLASH . 'ftgr.zip', $contents);
+		$zip = new ZipArchive;
+		$res = $zip->open(__DIR__ . FTGR_SLASH . 'ftgr.zip');
+		if ($res === TRUE)
+		{
+			$zip->extractTo(__DIR__ . FTGR_SLASH . 'ftgr');
+			$zip->close();
+		}
+		else
+		{
+			return array(FTGR_UNZIP_FAIL . $res);
+		}
+		$folder = __DIR__ . FTGR_SLASH . 'ftgr' . FTGR_SLASH . itemOf(scandir(__DIR__ . '/ftgr'), 2);
+		$scan = $this->update_recursive_scandir($folder);
+		$return = array();
+		foreach ($scan as $value)
+		{
+			$worked = copy($value, str_replace(FTGR_SLASH . 'includes' . FTGR_SLASH . 'classes' . FTGR_SLASH . 'ftgr' . FTGR_SLASH . 'Fightmon-the-Game--Reemon-dev' . FTGR_SLASH, FTGR_SLASH, $value));
+//$return[] = 'Copying ' . $value . ' to ' . str_replace(FTGR_SLASH .'includes'.FTGR_SLASH .'classes'.FTGR_SLASH .'ftgr'.FTGR_SLASH .'Fightmon-the-Game--Reemon-dev'.FTGR_SLASH, FTGR_SLASH, $value) . ' and it ' . ($worked ? 'worked' : 'did not work'); // For debug purposes
+		}
+		unlink(__DIR__ . FTGR_SLASH . 'ftgr.zip');
+		$this->update_remove_folder(__DIR__ . FTGR_SLASH . 'ftgr');
+//return $return; // For debug purposes
+		return FTGR_UPDATE_SUCCESS . str_replace('-', '.', $args[0]);
+	}
+
+	public function update_code($args = NULL)
+	{
+		if ($args === NULL)
+		{
+			return array(FTGR_REQUIRED_PARAM);
+		}
+		if ($args[0] === strtolower(FTGR_UPDATE_CODE))
+		{
+			return array(FTGR_UPDATE_CODE_ACCEPTED);
+			$_SESSION['ftgr']['valid_session'] = TRUE;
+		}
+		return array(FTGR_UPDATE_CODE_DENIED);
+	}
+
+	private function update_recursive_scandir($dir)
+	{
+		$result = array();
+		$root = scandir($dir);
+		foreach ($root as $value)
+		{
+			if ($value === '.' || $value === '..')
+			{
+				continue;
+			}
+			if (is_file("$dir" . FTGR_SLASH . "$value"))
+			{
+				$result[] = "$dir" . FTGR_SLASH . "$value";
+				continue;
+			}
+			foreach ($this->update_recursive_scandir("$dir" . FTGR_SLASH . "$value") as $value)
+			{
+				$result[] = $value;
+			}
+		}
+		return $result;
+	}
+
+	private function update_remove_folder($dir)
+	{
+		$files = array_diff(scandir($dir), array('.', '..'));
+		foreach ($files as $file)
+		{
+			(is_dir("$dir" . FTGR_SLASH . "$file")) ? $this->update_remove_folder("$dir" . FTGR_SLASH . "$file") : unlink("$dir" . FTGR_SLASH . "$file");
+		}
+		return rmdir($dir);
+	}
+
 	public function version($args = NULL)
 	{
 		$location = $args[0];
@@ -128,92 +214,6 @@ class fightmon
 			}
 		}
 		return array($latest_of_version);
-	}
-
-	public function update($args = NULL)
-	{
-		global $folder;
-		if (!$_SESSION['ftgr']['valid_session'])
-		{
-			return array(FTGR_UPDATE_REJECT);
-		}
-		if ($args === 'NULL')
-		{
-			return array(FTGR_REQUIRED_PARAM);
-		}
-		$url = trim('http://fightmon.eternityincurakai.com/ftgr/' . $args[0] . '.zip');
-		$contents = file_get_contents($url);
-		file_put_contents(__DIR__ . FTGR_SLASH . 'ftgr.zip', $contents);
-		$zip = new ZipArchive;
-		$res = $zip->open(__DIR__ . FTGR_SLASH . 'ftgr.zip');
-		if ($res === TRUE)
-		{
-			$zip->extractTo(__DIR__ . FTGR_SLASH . 'ftgr');
-			$zip->close();
-		}
-		else
-		{
-			return array(FTGR_UNZIP_FAIL . $res);
-		}
-		$folder = __DIR__ . FTGR_SLASH . 'ftgr' . FTGR_SLASH . itemOf(scandir(__DIR__ . '/ftgr'), 2);
-		$scan = $this->recursive_scandir($folder);
-		$return = array();
-		foreach ($scan as $value)
-		{
-			$worked = copy($value, str_replace(FTGR_SLASH . 'includes' . FTGR_SLASH . 'classes' . FTGR_SLASH . 'ftgr' . FTGR_SLASH . 'Fightmon-the-Game--Reemon-dev' . FTGR_SLASH, FTGR_SLASH, $value));
-//$return[] = 'Copying ' . $value . ' to ' . str_replace(FTGR_SLASH .'includes'.FTGR_SLASH .'classes'.FTGR_SLASH .'ftgr'.FTGR_SLASH .'Fightmon-the-Game--Reemon-dev'.FTGR_SLASH, FTGR_SLASH, $value) . ' and it ' . ($worked ? 'worked' : 'did not work'); // For debug purposes
-		}
-		unlink(__DIR__ . FTGR_SLASH . 'ftgr.zip');
-		$this->update_remove_folder(__DIR__ . FTGR_SLASH . 'ftgr');
-//return $return; // For debug purposes
-		return FTGR_UPDATE_SUCCESS . str_replace('-', '.', $args[0]);
-	}
-
-	public function update_code($args = NULL)
-	{
-		if ($args === NULL)
-		{
-			return array(FTGR_REQUIRED_PARAM);
-		}
-		if ($args[0] === strtolower(FTGR_UPDATE_CODE))
-		{
-			return array(FTGR_UPDATE_CODE_ACCEPTED);
-			$_SESSION['ftgr']['valid_session'] = TRUE;
-		}
-		return array(FTGR_UPDATE_CODE_DENIED);
-	}
-
-	private function recursive_scandir($dir)
-	{
-		$result = array();
-		$root = scandir($dir);
-		foreach ($root as $value)
-		{
-			if ($value === '.' || $value === '..')
-			{
-				continue;
-			}
-			if (is_file("$dir" . FTGR_SLASH . "$value"))
-			{
-				$result[] = "$dir" . FTGR_SLASH . "$value";
-				continue;
-			}
-			foreach ($this->recursive_scandir("$dir" . FTGR_SLASH . "$value") as $value)
-			{
-				$result[] = $value;
-			}
-		}
-		return $result;
-	}
-
-	private function update_remove_folder($dir)
-	{
-		$files = array_diff(scandir($dir), array('.', '..'));
-		foreach ($files as $file)
-		{
-			(is_dir("$dir" . FTGR_SLASH . "$file")) ? $this->update_remove_folder("$dir" . FTGR_SLASH . "$file") : unlink("$dir" . FTGR_SLASH . "$file");
-		}
-		return rmdir($dir);
 	}
 
 }
