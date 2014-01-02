46ac53795ed135de43fbb29923720c50dd445f2a
iggyvolz <iggyvolz@aim.com>
2014-01-02 15:43:50 -0500

    Added support for English (UK) - currently same as English (US)

--- includes/constants/index.php
+++ includes/constants/index.php
@@ -5,7 +5,7 @@ if (!defined('FTGR_MODE'))
 	define('FTGR_MODE', 'normal');
 }
 define('FTGR_VERSION', "0.3.0alpha");
-define('FTGR_LANG', 'en_US'); // en_US - English (US), fr_FR - French (France), es_ES - Spanish (Spain) [In progress]
+define('FTGR_LANG', 'en_US'); // en_US - English (US), en_GB - English (UK) [in progress], fr_FR - French (France) [in progress], es_ES - Spanish (Spain) [In progress]
 define('FTGR_DEBUG', TRUE); // If TRUE, allow the debug command to run
 define('FTGR_ALLOW_ANY_UPDATE', TRUE); // Allow anyone to run the update command.
 define('FTGR_UPDATE_CODE', NULL); // Put the update code in place of NULL to validate your session for update.
