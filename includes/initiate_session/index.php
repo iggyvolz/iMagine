cb88b7904c9124728c28f647a61cc81da043ca6e
iggyvolz <iggyvolz@aim.com>
2014-04-02 19:50:24 -0400

    Remove initiate_session() function, put code in manually

--- includes/initiate_session/index.php
+++ includes/initiate_session/index.php
@@ -1,8 +1,35 @@
-<?php  namespace ftgr;
+<?php
 
+namespace ftgr;
+
+$initial_session = array(
+	'me' => 'reemon',
+	'debug' => FALSE,
+	'init' => TRUE,
+	'returns' => array(),
+	'cutscene' => NULL,
+	'blazerenergy' => FTGR_BLAZER_STARTING_ENERGY,
+	'curleafenergy' => FTGR_CURLEAF_STARTING_ENERGY,
+	'dragirienergy' => FTGR_DRAGIRI_STARTING_ENERGY,
+	'feniixisenergy' => FTGR_FENIIXIS_STARTING_ENERGY,
+	'fireebeeenergy' => FTGR_FIREE_BEE_STARTING_ENERGY,
+	'flikeenergy' => FTGR_FLIKE_STARTING_ENERGY,
+	'ghostslicerenergy' => FTGR_GHOST_SLICER_STARTING_ENERGY,
+	'hartvileenergy' => FTGR_HARTVILE_STARTING_ENERGY,
+	'krabulousenergy' => FTGR_KRABULOUS_STARTING_ENERGY,
+	'nightwingenergy' => FTGR_NIGHTWING_STARTING_ENERGY,
+	'plantsyenergy' => FTGR_PLANTSY_STARTING_ENERGY,
+	'pluffenergy' => FTGR_PLUFF_STARTING_ENERGY,
+	'reebeeenergy' => FTGR_REE_BEE_STARTING_ENERGY,
+	'reemonenergy' => FTGR_REEMON_STARTING_ENERGY,
+	'skelestormenergy' => FTGR_SKELESTORM_STARTING_ENERGY,
+	'strabenergy' => FTGR_STRAB_STARTING_ENERGY,
+	'version' => FTGR_VERSION,
+	'valid_session' => FTGR_ALLOW_ANY_UPDATE,
+);
 if (!isset($_SESSION['ftgr']['init']))
 {
-	init_session();
+	$_SESSION['ftgr'] = $initial_session;
 }
 
 if ($_SESSION['ftgr']['version'] !== FTGR_VERSION)
@@ -15,34 +42,7 @@ if ($_SESSION['ftgr']['version'] !== FTGR_VERSION)
 	else
 	{
 		session_destroy();
-		init_session();
+		session_start();
+		$_SESSION['ftgr'] = $initial_session;
 	}
-}
-
-function init_session()
-{
-	$_SESSION['ftgr'] = array();
-	$_SESSION['ftgr']['me'] = 'reemon';
-	$_SESSION['ftgr']['debug'] = FALSE;
-	$_SESSION['ftgr']['init'] = TRUE;
-	$_SESSION['ftgr']['returns'] = array();
-	$_SESSION['ftgr']['cutscene'] = NULL;
-	$_SESSION['ftgr']['blazerenergy'] = FTGR_BLAZER_STARTING_ENERGY;
-	$_SESSION['ftgr']['curleafenergy'] = FTGR_CURLEAF_STARTING_ENERGY;
-	$_SESSION['ftgr']['dragirienergy'] = FTGR_DRAGIRI_STARTING_ENERGY;
-	$_SESSION['ftgr']['feniixisenergy'] = FTGR_FENIIXIS_STARTING_ENERGY;
-	$_SESSION['ftgr']['fireebeeenergy'] = FTGR_FIREE_BEE_STARTING_ENERGY;
-	$_SESSION['ftgr']['flikeenergy'] = FTGR_FLIKE_STARTING_ENERGY;
-	$_SESSION['ftgr']['ghostslicerenergy'] = FTGR_GHOST_SLICER_STARTING_ENERGY;
-	$_SESSION['ftgr']['hartvileenergy'] = FTGR_HARTVILE_STARTING_ENERGY;
-	$_SESSION['ftgr']['krabulousenergy'] = FTGR_KRABULOUS_STARTING_ENERGY;
-	$_SESSION['ftgr']['nightwingenergy'] = FTGR_NIGHTWING_STARTING_ENERGY;
-	$_SESSION['ftgr']['plantsyenergy'] = FTGR_PLANTSY_STARTING_ENERGY;
-	$_SESSION['ftgr']['pluffenergy'] = FTGR_PLUFF_STARTING_ENERGY;
-	$_SESSION['ftgr']['reebeeenergy'] = FTGR_REE_BEE_STARTING_ENERGY;
-	$_SESSION['ftgr']['reemonenergy'] = FTGR_REEMON_STARTING_ENERGY;
-	$_SESSION['ftgr']['skelestormenergy'] = FTGR_SKELESTORM_STARTING_ENERGY;
-	$_SESSION['ftgr']['strabenergy'] = FTGR_STRAB_STARTING_ENERGY;
-	$_SESSION['ftgr']['version'] = FTGR_VERSION;
-	$_SESSION['ftgr']['valid_session'] = FTGR_ALLOW_ANY_UPDATE;
 }
