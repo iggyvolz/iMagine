1139635101b97a3c43239a271ff058e296fa074c
iggyvolz <iggyvolz@aim.com>
2014-04-17 17:14:56 -0400

    Add testDebug()

--- tests/APITest.php
+++ tests/APITest.php
@@ -25,12 +25,27 @@ class APITest extends PHPUnit_Framework_TestCase
 	public function testDebug()
 	{
 		global $blazer;
-		$test = new PHPUnitTest($this, "Testing debug()", __METHOD__, 2);
+		$test = new PHPUnitTest($this, "Testing debug()", __METHOD__, 10);
 		if (!FTGR_DEBUG)
 		{
 			$test->skipTest("Please turn on debug mode to accurately test this function.");
 			return;
 		}
+		$output = $blazer->debug(['on']);
+		$test->assertEquals($output, ['Debug mode is now on.']);
+		$test->assertTrue($_SESSION['ftgr']['debug']);
+		$output = $blazer->debug(['']);
+		$test->assertEquals($output, ['Debug mode is now off.']);
+		$test->assertFalse($_SESSION['ftgr']['debug']);
+		$output = $blazer->debug(['']);
+		$test->assertEquals($output, ['Debug mode is now on.']);
+		$test->assertTrue($_SESSION['ftgr']['debug']);
+		$output = $blazer->debug(['on']);
+		$test->assertEquals($output, ['Debug mode is now on.']);
+		$test->assertTrue($_SESSION['ftgr']['debug']);
+		$output = $blazer->debug(['']);
+		$test->assertEquals($output, ['Debug mode is now off.']);
+		$test->assertFalse($_SESSION['ftgr']['debug']);
 	}
 
 }
