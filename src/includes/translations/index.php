1e9df6c2a898e799fc1d88b112ebc22f91650b57
iggyvolz <iggyvolz@aim.com>
2014-04-26 15:52:29 -0400

    More debugging

--- src/includes/translations/index.php
+++ src/includes/translations/index.php
@@ -7,17 +7,19 @@ if (function_exists("textdomain")) // Run only if gettext is enabled
 	putenv('LC_ALL=' . FTGR_LANG);
 	setlocale(LC_ALL, FTGR_LANG);
 
-	bindtextdomain("ftgr_".FTGR_VERSION, __DIR__);
+	bindtextdomain("ftgr_" . FTGR_VERSION, __DIR__);
 
-	textdomain("ftgr_".FTGR_VERSION);
+	textdomain("ftgr_" . FTGR_VERSION);
 
 
-	bind_textdomain_codeset("ftgr_".FTGR_VERSION, 'UTF-8'); // Use UTF-8
+	bind_textdomain_codeset("ftgr_" . FTGR_VERSION, 'UTF-8'); // Use UTF-8
 }
-if(!function_exists("_"))
+if (!function_exists("ftgr\_"))
 {
-function _($return)
-{
-	return $return;
-}
+
+	function _($return)
+	{
+		return $return;
+	}
+
 }
