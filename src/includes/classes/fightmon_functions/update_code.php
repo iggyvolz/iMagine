1cb649a50a4de555a195743fed64d2dad66fee28
iggyvolz <iggyvolz@aim.com>
2014-05-08 20:10:55 -0400

    Mark update_code as unstable in alpha

--- src/includes/classes/fightmon_functions/update_code.php
+++ src/includes/classes/fightmon_functions/update_code.php
@@ -6,6 +6,7 @@ trait update_code
 {
 	public function update_code($args = NULL)
 	{
+		return array(\ftgr\_("WARNING - The Update function has been removed until further notice due to changes in FTG:R's structure.  Please check the dev-update branch for development of this feature."));
 		if ($args === NULL)
 		{
 			return array(\ftgr\_('This function requires a parameter.  Please see the documentation.'));
