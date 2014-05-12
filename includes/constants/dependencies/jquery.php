36529d2175dbe5bc64bb21804a38a24e5cb02f3f
iggyvolz <iggyvolz@aim.com>
2014-05-12 19:54:44 -0400

    Revert "Update jQuery to (1.11/2.1).1"
    This reverts commit 9e0527537e86468071c4f15bd19c6dc6deb34270.

--- includes/constants/dependencies/jquery.php
+++ includes/constants/dependencies/jquery.php
@@ -2,6 +2,6 @@
 
 namespace ftgr;
 
-define('FTGR_JQUERY_1X_VERSION', '1.11.1');
-define('FTGR_JQUERY_2X_VERSION', '2.1.1');
+define('FTGR_JQUERY_1X_VERSION', '1.11.0');
+define('FTGR_JQUERY_2X_VERSION', '2.1.0');
 define('FTGR_JQUERY_URL', '//ajax.googleapis.com/ajax/libs/jquery/%JQUERY_VERSION%/jquery.min.js');
