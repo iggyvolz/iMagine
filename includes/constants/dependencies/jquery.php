5ddb141970fbe43c53bb43c02924961ebcd7d17e
iggyvolz <iggyvolz@aim.com>
2014-10-28 19:48:24 +0000

    Update jQuery

--- includes/constants/dependencies/jquery.php
+++ includes/constants/dependencies/jquery.php
@@ -2,6 +2,6 @@
 
 namespace iMagine;
 
-define('IMAGINE_JQUERY_1X_VERSION', '1.11.0');
-define('IMAGINE_JQUERY_2X_VERSION', '2.1.0');
+define('IMAGINE_JQUERY_1X_VERSION', '1.11.1');
+define('IMAGINE_JQUERY_2X_VERSION', '2.1.1');
 define('IMAGINE_JQUERY_URL', '//ajax.googleapis.com/ajax/libs/jquery/%JQUERY_VERSION%/jquery.min.js');
