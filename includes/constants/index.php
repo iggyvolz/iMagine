84aa074b6112a0ee01a165109dd61b9fc6fb08ba
iggyvolz <iggyvolz@aim.com>
2014-11-01 15:18:17 -0400

    Set starting energy to 1000 not 100

--- includes/constants/index.php
+++ includes/constants/index.php
@@ -11,7 +11,7 @@ define('IMAGINE_LANG', 'en_US'); // en_US - English (US), fr_FR - French (France
 define('IMAGINE_DEBUG', TRUE); // If TRUE, allow the debug command to run
 define('IMAGINE_ALLOW_ANY_UPDATE', TRUE); // Allow anyone to run the update command.
 define('IMAGINE_UPDATE_CODE', NULL); // Put the update code in place of NULL to validate your session for update.
-define("IMAGINE_TONY_STARTING_ENERGY", 100);
-define("IMAGINE_EDYN_STARTING_ENERGY", 100);
-define("IMAGINE_STRAG_STARTING_ENERGY", 100);
+define("IMAGINE_TONY_STARTING_ENERGY", 1000);
+define("IMAGINE_EDYN_STARTING_ENERGY", 1000);
+define("IMAGINE_STRAG_STARTING_ENERGY", 1000);
 require_once realpath(__DIR__ . '/dependencies/index.php');
