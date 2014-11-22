f39a6c2adf772da8d45098999788449fa6779b26
Eternity Incurakai Server <admin@eternityincurakai.com>
2014-11-22 13:21:11 -0500

    No need for test security

--- includes/classes/imagine_functions/test.php
+++ includes/classes/imagine_functions/test.php
@@ -6,10 +6,6 @@ trait test
 {
 	public function test($args = NULL)
 	{
-		if (!$_SESSION['iMagine']['valid_session'])
-		{
-			return array(\iMagine\_('You cannot test.  Please enter the auth code with auth_code()'));
-		}
 		return explode("\n",`make test`);
 	}
 }
