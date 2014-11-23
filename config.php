d7f58d3bc50545b8e511eb2506361b5988f1b0c1
iggyvolz <iggyvolz@gmail.com>
2014-11-22 21:06:00 -0500

    Don't define IMAGINE_API_URL by default

--- config.php
+++ config.php
@@ -1,4 +1,6 @@
 <?php
 // WARNING: make sure you git update-index --assume-unchanged config.php UNLESS you are making changes to the template
 
-define("IMAGINE_API_URL","");
+// Give your URL for testing:
+//define("IMAGINE_API_URL","");
+
