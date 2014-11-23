91fda2b58a833b483c766c1624e7ecbde1d289e8
iggyvolz <iggyvolz@gmail.com>
2014-11-22 22:00:47 -0500

    Revert "Require git"
    This reverts commit 97060b500a50f603d225aee112bc28ff49f41c58.

--- includes/classes/imagine_functions/version.php
+++ includes/classes/imagine_functions/version.php
@@ -7,6 +7,14 @@ trait version
 
 	public function version($person=NULL,...$excess)
 	{
+		if(!is_dir(__DIR__."/../../../.git"))
+		{
+			return array(sprintf(\iMagine\_("On version %s."),IMAGINE_VERSION));
+		}
+		if(`git 2>&1|grep "command not found"`!==NULL)
+		{
+			return array(sprintf(\iMagine\_("On version %s."),IMAGINE_VERSION));
+		}
 		$describe=`git describe --tags|tr -d '\n'`;
 		if(strpos($describe,"-")===FALSE)
 		{
