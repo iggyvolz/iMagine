2c4a21057fa3ee811db7aebafafa7d2ca1096172
iggyvolz <iggyvolz@aim.com>
2015-07-07 19:44:51 -0400

    By default no update code

--- config.default.php
+++ config.default.php
@@ -6,7 +6,7 @@ define('IMAGINE_VERSION', "development");
 define('IMAGINE_LANG', 'en_US'); // en_US - English (US), fr_FR - French (France), es_ES - Spanish (Spain) [In progress]
 define('IMAGINE_DEBUG', FALSE); // If TRUE, allow the debug command to run
 define('IMAGINE_ALLOW_ANY_UPDATE', FALSE); // Allow anyone to run the update command.
-define('IMAGINE_UPDATE_CODE', 'abc123'); // Put the update code in place of NULL to validate your session for update.
+define('IMAGINE_UPDATE_CODE', null); // Put the update code in place of NULL to validate your session for update.
 define("IMAGINE_TONY_STARTING_ENERGY", 1000);
 define("IMAGINE_EDYN_STARTING_ENERGY", 1000);
 define("IMAGINE_STRAG_STARTING_ENERGY", 1000);
