cf3c633a165454b59eabac7e099e8960c9804816
iggyvolz <iggyvolz@aim.com>
2015-07-07 22:11:44 -0400

    Can't send creature to dream plane twice

--- includes/classes/dreamcreatures/functions/tothedreamplane.php
+++ includes/classes/dreamcreatures/functions/tothedreamplane.php
@@ -6,6 +6,10 @@ trait tothedreamplane
 {
 	public function tothedreamplane(...$excess)
 	{
+		if($this->indreamplane)
+		{
+			return [sprintf(\iMagine\_("%s: %s is already in the dream plane!"),\iMagine\_(ucfirst($this->owner)),\iMagine\_(ucfirst(explode("\\",get_class($this))[1])))];
+		}
 		$this->indreamplane=true;
 		return [sprintf(\iMagine\_("%s: %s, to the dream plane!"),\iMagine\_(ucfirst($this->owner)),\iMagine\_(ucfirst(explode("\\",get_class($this))[1])))];
 	}
