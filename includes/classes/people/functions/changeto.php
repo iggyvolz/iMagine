e1176127a459be5d1fc5faebdedbb3c6194bc525
iggyvolz <iggyvolz@aim.com>
2015-07-07 22:15:39 -0400

    changeto should set me to lowercase name

--- includes/classes/people/functions/changeto.php
+++ includes/classes/people/functions/changeto.php
@@ -7,7 +7,7 @@ trait changeto
 	public function changeto(...$excess)
 	{
 		global $iMagine;
-		$iMagine->me = explode('\\', get_class($this))[1];
+		$iMagine->me = strtolower(explode('\\', get_class($this))[1]);
 		return array(sprintf(\iMagine\_('%s is now selected!'), ucfirst(explode('\\', get_class($this))[1])));
 	}
 }
