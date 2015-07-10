a98158a742ea350890515797cfbbb12ec5299518
iggyvolz <iggyvolz@aim.com>
2015-07-10 13:39:44 -0400

    Add reset

--- js/init.js
+++ js/init.js
@@ -19,31 +19,42 @@ function reload() {
     $('#results').val($('#results').val()+"\n>"+$('#contents').val());
     var conts=$('#contents').val();
     $('#contents').val("");
+    var handled=false;
     if (conts === "help") {
         window.location.replace("help.html");
+        handled=true;
+    }
+    if (conts === "reset") {
+        localStorage.iMagineUID=null;
+        localStorage.iMagineResults=null;
+        window.location.reload();
+        handled=true;
     }
     if (localStorage)
     {
       localStorage.iMagineResults=$('#results').val();
     }
-    $.get('http://i.magine.tk/head.php', "command=" + conts + "&uid=" + uid, function(data) {
-        $('#results').val($('#results').val()+"\n"+data.response);
-        $('#results').scrollTop($('#results').prop("scrollHeight"));
-        $('#tony_energy').val(data.tony_energy);
-        $('#edyn_energy').val(data.edyn_energy);
-        $('#strag_energy').val(data.strag_energy);
-        $('#tony_energy_disp').html(data.tony_energy);
-        $('#edyn_energy_disp').html(data.edyn_energy);
-        $('#strag_energy_disp').html(data.strag_energy);
-        console.log(data);
-        $('#errors').html(data.errors);
-    }, "json").fail(function(data) {
-        console.log(data);
-        $('#errors').html(data.responseText);
-        $('#contents').val("");
-    });
-    if (localStorage)
+    if(!handled)
     {
-      localStorage.iMagineResults=$('#results').val();
+      $.get('http://i.magine.tk/head.php', "command=" + conts + "&uid=" + uid, function(data) {
+          $('#results').val($('#results').val()+"\n"+data.response);
+          $('#results').scrollTop($('#results').prop("scrollHeight"));
+          $('#tony_energy').val(data.tony_energy);
+          $('#edyn_energy').val(data.edyn_energy);
+          $('#strag_energy').val(data.strag_energy);
+          $('#tony_energy_disp').html(data.tony_energy);
+          $('#edyn_energy_disp').html(data.edyn_energy);
+          $('#strag_energy_disp').html(data.strag_energy);
+          console.log(data);
+          $('#errors').html(data.errors);
+          if (localStorage)
+          {
+            localStorage.iMagineResults=$('#results').val();
+          }
+      }, "json").fail(function(data) {
+          console.log(data);
+          $('#errors').html(data.responseText);
+          $('#contents').val("");
+      });
     }
 }
